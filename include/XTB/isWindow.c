145a9cabb8ffaf727483679f9f15386d15acd20a
Filip Kristan <252480936+filipkristan@users.noreply.github.com>
2026-09-17 20:30:10 +0200

    Fix getAllWIDs crashing out when the window doesn't exist.

--- include/XTB/isWindow.c
+++ include/XTB/isWindow.c
@@ -3,16 +3,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Supress error
+static int x_error_handler(Display *dpy, XErrorEvent *event) {
+    return 0;
+}
 // NOTES: Add error handling!
 bool isWindow(const char *wid) {
     Display *display = XOpenDisplay(NULL);
     if (!display) {
-        fprintf(stderr, "Cannot open display\n");
         return false;
     }
-    Window window = strtoul(wid,NULL, 0);
+
+    Window window = strtoul(wid, NULL, 0);
     XWindowAttributes winAttrib;
+
+    XErrorHandler old_handler = XSetErrorHandler(x_error_handler);
+
     Status res = XGetWindowAttributes(display, window, &winAttrib);
+
+    XSetErrorHandler(old_handler);
+
     XCloseDisplay(display);
     return res != 0;
 }
--- src/main.c
+++ src/main.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 #include <XTB/xtb.h>
 
-void main() {
+char storage[32][32];
+
+int main() {
     Display *display = XOpenDisplay(NULL);
     if (!display) {
         fprintf(stderr, "Cannot open display\n");
     }
-    RGB color = getColorAt(0,0);
-    printf("%d", color.r);
+    // Make a list of all running window IDs
+    getAllWIDs(storage);
+    for (int i = 0; i < sizeof(storage) / sizeof(storage[0]); ++i) {
+        if (strcmp(storage[i], "\0") != 0) {
+
+            if (isWindow(storage[i])) {
+                printf("Found window for: %s\n", storage[i]);
+            } else {
+                printf("Couldn't find window: %s\n", storage[i]);
+                printf("%hhd", isWindow(storage[i]));
+            }
+            // printf("%s\n", getWindowTitle( storage[i]));            0x02200097
+            //printf("%s\n", getWindowTitle("0x02200097"));
+        }
+    }
 }
